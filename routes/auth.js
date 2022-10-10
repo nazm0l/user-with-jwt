aa5367bf971698f6d96f16aa897eeab5251dd28b
nazm0l <tanjirahmednazmul@gmail.com>
2022-10-10 19:52:39 +0600

    validation added

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,22 @@
 const router = require("express").Router();
+const { check, validationResult } = require("express-validator");
 
-router.post("/signup", (req, res) => {
-  const { email, password } = req.body;
-  console.log(email, password);
-  res.send("got it");
-});
+router.post(
+  "/signup",
+  [check("email").isEmail(), check("password").isLength({ min: 6 })],
+  (req, res) => {
+    const { email, password } = req.body;
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+
+    console.log(email, password);
+    res.send("got it");
+  }
+);
 
 module.exports = router;
