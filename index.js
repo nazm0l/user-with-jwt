eedbef093be862933d08793abd87faecfb29541d
nazm0l <tanjirahmednazmul@gmail.com>
2022-10-10 19:37:24 +0600

    signup route created

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const auth = require("./routes/auth");
+
 const app = express();
+
+app.use(express.json());
+app.use("/auth", auth);
 
 app.get("/", (req, res) => {
   res.send("Yes your app is working");
--- routes/auth.js
+++ routes/auth.js
@@ -1,1 +1,9 @@
-const router = 
+const router = require("express").Router();
+
+router.post("/signup", (req, res) => {
+  const { email, password } = req.body;
+  console.log(email, password);
+  res.send("got it");
+});
+
+module.exports = router;
